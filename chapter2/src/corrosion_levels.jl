8952919ebdfae133825814e59b5e19f6b60ced5f
Ferdinand Willemin <ferdi.willemin@gmail.com>
2024-01-18 18:04:28 -0500

    julia script

--- chapter2/src/corrosion_levels.jl
+++ chapter2/src/corrosion_levels.jl
@@ -1,6 +1,8 @@
 using Optim
 using Distributions
 using ForwardDiff
+using CSV
+using DataFrames, NLSolversBase
 
 include("utils/utils.jl")
 
@@ -55,9 +57,42 @@ function mean_failure_time(corrosion_level, parameters, var_cov_matrix, to_round
     return mean_failure_time
 end
 
-function deviance(func, parameters_m1, func_0, parameters_m0)
+function deviance(func, parameters_m1, func_m0, parameters_m0)
     opt_1 = optimize(func, parameters_m1).minimum
-    opt_0 = optimize(func_0, parameters_m0).minimum
+    opt_0 = optimize(func_m0, parameters_m0).minimum
 
     return round(2 * (opt_0 - opt_1); digits=2)
 end
+
+# Script
+
+engine_df = CSV.read(joinpath(@__DIR__, "../data/engine.csv"), DataFrame)
+
+n = length(engine_df[!, :CorrosionLevel])
+function log_likelihood(betas, W, T)
+    llike = n * log(betas[1]) + betas[2] * sum(log.(W)) - betas[1] * sum(W .^ betas[2] .* T)
+    llike = -llike
+end
+
+function log_likelihood_m0(betas, T)
+    llike = n * log(betas[1]) - betas[1] * sum(T)
+    llike = -llike
+end
+
+W = reshape(engine_df[!, :CorrosionLevel], :, 1)
+T = reshape(engine_df[!, :Lifetime], :, 1)
+corrosion_level = 3
+
+nvar = 2
+func = TwiceDifferentiable(vars -> log_likelihood(vars[1:nvar], W, T),
+    ones(nvar); autodiff=:forward)
+func_m0 = TwiceDifferentiable(vars -> log_likelihood_m0(vars[1:1], T),
+    ones(1); autodiff=:forward)
+
+result = parameter_estimation(func, [1.0, 1.0], true)
+println("Parameters estimations : ", result.estimates)
+println("variance-covariance matrix: ", result.var_cov_matrix)
+println("Standard-deviation : ", result.std)
+println("Confidence intervals : ", result.intervals)
+println("Mean failure time : ", mean_failure_time(corrosion_level, result.estimates, result.var_cov_matrix, true))
+println("Deviance : ", deviance(func, [1.0, 1.0], func_m0, [1.0]))
