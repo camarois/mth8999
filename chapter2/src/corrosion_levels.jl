a9e0e5ecf4d549c8e3a276e344e3a1969afee847
Ferdinand Willemin <ferdi.willemin@gmail.com>
2024-01-18 17:52:13 -0500

    deviance

--- chapter2/src/corrosion_levels.jl
+++ chapter2/src/corrosion_levels.jl
@@ -25,13 +25,13 @@ function parameter_estimation(func, initial_parameters, to_round=false)
     intervals = []
     for i in eachindex(parameters)
         parameter_95 = confidence_intervals_0_95(parameters[i], std[i])
-        
+
         if to_round
             parameter_95 = round.(parameter_95; digits=ROUND_DIGITS)
         end
 
         append!(intervals, [parameter_95])
-    end 
+    end
 
     if to_round
         parameters = round.(parameters; digits=ROUND_DIGITS)
@@ -43,14 +43,21 @@ end
 
 function mean_failure_time(corrosion_level, parameters, var_cov_matrix, to_round=false)
     lambda = parameters[1]^-1 * corrosion_level^(-parameters[2])
-    delta = [(-parameters[1]^(-2))*corrosion_level^(-parameters[2]);(-parameters[1]^(-1))*corrosion_level^(-parameters[2])*log(corrosion_level)]
+    delta = [(-parameters[1]^(-2)) * corrosion_level^(-parameters[2]); (-parameters[1]^(-1)) * corrosion_level^(-parameters[2]) * log(corrosion_level)]
     deltaT = transpose(delta)
     var = deltaT * var_cov_matrix * delta
 
     mean_failure_time = confidence_intervals_0_95(lambda, sqrt(var))
     if to_round
-        mean_failure_time = round.(mean_failure_time;digits=ROUND_DIGITS)
+        mean_failure_time = round.(mean_failure_time; digits=ROUND_DIGITS)
     end
 
     return mean_failure_time
+end
+
+function deviance(func, parameters_m1, func_0, parameters_m0)
+    opt_1 = optimize(func, parameters_m1).minimum
+    opt_0 = optimize(func_0, parameters_m0).minimum
+
+    return round(2 * (opt_0 - opt_1); digits=2)
 end
--- chapter2/test/test_corrosion_levels.jl
+++ chapter2/test/test_corrosion_levels.jl
@@ -2,21 +2,28 @@ using CSV
 using DataFrames, NLSolversBase
 
 @testset "corrosion_levels.jl" begin
-   # Inspired from optim documentation https://julianlsolvers.github.io/Optim.jl/stable/#examples/generated/maxlikenlm/
+    # Inspired from optim documentation https://julianlsolvers.github.io/Optim.jl/stable/#examples/generated/maxlikenlm/
     engine_df = CSV.read(joinpath(@__DIR__, "../data/engine.csv"), DataFrame)
 
-    n = length(engine_df[!,:CorrosionLevel])
+    n = length(engine_df[!, :CorrosionLevel])
     function log_likelihood(betas, W, T)
-        llike = n*log(betas[1]) + betas[2] * sum(log.(W)) - betas[1] * sum(W.^betas[2] .* T)
+        llike = n * log(betas[1]) + betas[2] * sum(log.(W)) - betas[1] * sum(W .^ betas[2] .* T)
         llike = -llike
     end
 
-    W = reshape(engine_df[!,:CorrosionLevel], :, 1)
-    T = reshape(engine_df[!,:Lifetime], :, 1)
+    function log_likelihood_m0(betas, T)
+        llike = n * log(betas[1]) - betas[1] * sum(T)
+        llike = -llike
+    end
 
-    nvar=2
+    W = reshape(engine_df[!, :CorrosionLevel], :, 1)
+    T = reshape(engine_df[!, :Lifetime], :, 1)
+
+    nvar = 2
     func = TwiceDifferentiable(vars -> log_likelihood(vars[1:nvar], W, T),
-                               ones(nvar); autodiff=:forward);
+        ones(nvar); autodiff=:forward)
+    func_m0 = TwiceDifferentiable(vars -> log_likelihood_m0(vars[1:1], T),
+        ones(1); autodiff=:forward)
 
 
     @testset "parameter_estimation()" begin
@@ -27,7 +34,7 @@ using DataFrames, NLSolversBase
             @test result.estimates == [1.133, 0.4792]
             @test result.var_cov_matrix == [0.0468 -0.0144; -0.0144 0.0311]
             @test result.std == [0.2164, 0.1762]
-            @test result.intervals == [[0.7089, 1.5572],[0.1338, 0.8246]]
+            @test result.intervals == [[0.7089, 1.5572], [0.1338, 0.8246]]
         end
     end
 
@@ -37,6 +44,14 @@ using DataFrames, NLSolversBase
             corrosion_level = 3
             result = parameter_estimation(func, [1.0, 1.0])
             @test mean_failure_time(corrosion_level, result.estimates, result.var_cov_matrix, true) == [0.3023, 0.7404]
+        end
+    end
+
+    @testset "deviance()" begin
+        @testset "valid_values()" begin
+
+            @test deviance(func, [1.0, 1.0], func_m0, [1.0]) == 11.16
+
         end
     end
 end
